b9b2ab730a682267937658dd7789e7cd27de4efb
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 04:19:23 -0500

    Update power_main.cpp

--- power/power_main.cpp
+++ power/power_main.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
-
+	int base, expo;
+	cout << "Enter your base number" <<endl;
+	cin >> base;
+	cout << "Enter your exponent, make sure its not negative" << endl;
+	cin >> expo;
+	
+	int result = power(base,expo);
+	cout << result << endl;
 
 	return 0;
+}
+
+int power(int base, int expo)
+{
+	if(expo == 0)
+	{
+		return 1;
+	}
+	else if (expo % 2 == 0)
+	{
+		return base * base;
+	}
+	else 
+	{
+		return base * power(base, (expo - 1));
+	}
 }
